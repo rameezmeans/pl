a59bc554d7a65d5e29c0c27d38773f6ef6b636c1
Rameez Bhatti <xrkalix@gmail.com>
2024-11-05 10:12:57 +0500

    v1 v2

--- resources/views/files/show_file.blade.php
+++ resources/views/files/show_file.blade.php
@@ -1058,7 +1058,7 @@ div.file-type-buttons label > input + img {
                 <i class="fa fa-upload top-box top-box-green"></i>
               </span>
               <h3 style="display: inline; color: #237E02; margin-left: 20px;">
-                File Received
+                File Received {{substr($row->request_file, -2)}}
               </h3>
               <span class="r-file-span" style="float: right; margin-top: 15px;">{{ $row->created_at->format('d/m/Y')}} at {{$row->created_at->format('H:i:s')}}</span>
             
@@ -1369,7 +1369,7 @@ div.file-type-buttons label > input + img {
               <i class="fa fa-upload top-box top-box-green"></i>
             </span>
             <h3 style="display: inline; color: #237E02; margin-left: 20px;">
-              File Received
+              File Received {{substr($row->request_file, -2)}}
             </h3>
             <span class="r-file-span" style="float: right; margin-top: 15px;">{{ $row->created_at->format('d/m/Y')}} at {{$row->created_at->format('H:i:s')}}</span>
           
@@ -2024,7 +2024,7 @@ div.file-type-buttons label > input + img {
                 <i class="fa fa-upload top-box top-box-green"></i>
               </span>
               <h3 style="display: inline; color: #237E02; margin-left: 20px;">
-                File Received
+                File Received {{substr($row->request_file, -2)}}
               </h3>
               <span style="float: right; margin-top: 15px;">{{ $row->created_at->format('d/m/Y')}} at {{$row->created_at->format('H:i:s')}}</span>
             
