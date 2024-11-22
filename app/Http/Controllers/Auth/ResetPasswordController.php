3c44862997b5651183a2ea61473fbfa5cf6fd48e
Rameez Bhatti <xrkalix@gmail.com>
2024-11-22 15:58:52 +0500

    reset thing

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use ECUApp\SharedCode\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Support\Str;
 
 class ResetPasswordController extends Controller
 {
@@ -26,4 +29,21 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+
+
+    protected function resetPassword($user, $password)
+    {
+        $userActual = User::where('email', $user->email)->where('front_end_id', 1)->first();
+
+        $this->setUserPassword($userActual, $password);
+
+        $userActual->setRememberToken(Str::random(60));
+
+        $userActual->save();
+
+        event(new PasswordReset($userActual));
+
+        $this->guard()->login($userActual);
+    }
+
 }
