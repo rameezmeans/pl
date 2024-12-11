e41438b5e58cafc396324be14003fab8e443991d
Rameez Bhatti <xrkalix@gmail.com>
2024-12-11 21:58:36 +0500

    file support

--- resources/views/files/show_file.blade.php
+++ resources/views/files/show_file.blade.php
@@ -2094,10 +2094,13 @@ div.file-type-buttons label > input + img {
             </div>
 
             @endif
-            
+
            {{-- here are are --}}
            {{-- done --}}
            {{-- again--}}
+
+
+            {{-- @if(!$file->messages_and_logs()->isEmpty())
 
             <div class="main-file-box m-t-40">
               <span>
