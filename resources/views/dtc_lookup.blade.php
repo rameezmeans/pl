022df1c8480ab734e5bf20f774ccebaf1b98a46d
Rameez Bhatti <xrkalix@gmail.com>
2025-02-12 19:27:46 +0500

    here we are

--- resources/views/dtc_lookup.blade.php
+++ resources/views/dtc_lookup.blade.php
@@ -8,17 +8,17 @@
       <div class="container-fluid">
         <div class="bb-light fix-header">
                 <div class="header-block header-block-w-p">
-                    <h1>DTC Lookup Records</h1>
-                    <p>Search DTC Lookup records by Code</p>
+                    <h1>{{translate('DTC Lookup Records')}}</h1>
+                    <p>{{translate('Search DTC Lookup records by Code')}}</p>
                 </div>
             </div>
         
         <div class="i-content-block price-level">
           <form action="{{route('get-dtc-desc')}}" method="POST" class="text-center">
             @csrf
-            <input type="text" name="dtc_lookup_code" value="" class="form-control text-center" placeholder="Enter DTC Code">
+            <input type="text" name="dtc_lookup_code" value="" class="form-control text-center" placeholder="{{translate('Enter DTC Code')}}">
             <div>
-              <button class="btn btn-red btn-red-full text-center m-t-10" type="submit">GET DESCRIPTION</button>
+              <button class="btn btn-red btn-red-full text-center m-t-10" type="submit">{{translate('GET DESCRIPTION')}}</button>
             </div>
           </form>
 
@@ -44,7 +44,7 @@
 
                   <div class="card-header">
                     <div style="display: inline-flex;">
-                      <h4>No Record Found!</h4>
+                      <h4>{{translate('No Record Found!')}}</h4>
                     </div>
                   </div>
 
